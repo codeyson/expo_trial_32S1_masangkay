49693fb8f47937c135d40635266192db821b16e4
Fred <frederickdianzon@gmail.com>
2026-03-27 16:24:11 +0800

    done up to Exploring Layouts and Flexbox

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Your Course Goal' style={styles.textInput}></TextInput>
+        <Button title='Add Goal'></Button>
+      </View>
+      <View>
+          <Text>List of Course Goals</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +21,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    margin: 16, 
+    borderWidth: 2, 
+    borderColor: 'red', 
+    padding: 16,
+    backgroundColor: 'blue',
+  },
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },  
+  textInput: {
+    borderWidth: 2,
+    color: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 3,
+  }
 });
+
+
+const styles2 = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  textStyle: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'red',
+    backgroundColor: 'blue',
+    color: 'white',
+  },
+};
+
